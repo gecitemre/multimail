af37b80c9f3722c22c6bdc9929a275e33ae0bec5
Emre Geçit <emregecit5@gmail.com>
2026-02-04 00:14:38 +0300

    Initial Commit

---
+++ MultiMail/MultiMailApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiMailApp.swift
+//  MultiMail
+//
+//  Created by Emre Geçit on 4.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct MultiMailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
